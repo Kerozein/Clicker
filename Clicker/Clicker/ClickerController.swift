86eb5cb53d3ad4c2d3bf6d6991243fb7db9598ed
Tom <tomkowalpicchi@gmail.com>
2023-04-03 19:37:17 +0200

    clicker core functions

--- Clicker/Clicker/ClickerController.swift
+++ Clicker/Clicker/ClickerController.swift
@@ -7,27 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClickerViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var health: UIProgressView!
+    @IBOutlet weak var Monster: UIButton!
+    var baseDamage:Float = 0.1
     var money:Int = 0
     var timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(self.timerFun), userInfo: nil, repeats: true)
-
-        
+        changeMonster()
+        updateMoneyLabel()
     }
     @objc func timerFun(){
         decreaseQuality()
-        increaseMoney()
-        print(money)
         return
     }
     
     func decreaseQuality(){}
-    func increaseMoney(){money+=100}
-
+    func increaseMoney(){
+        money+=100
+    }
+    func updateMoneyLabel(){moneyLabel.text = "Money : \(money)$"}
+    
+    
+    @IBAction func onHit(_ sender: Any) {
+        print("hit")
+        health.progress -= baseDamage
+        if(health.progress == 0.0){
+            changeMonster()
+            increaseMoney()
+            updateMoneyLabel()
+        }
+    }
+    func changeMonster(){
+        let numberOfMonsters:Int = 2
+        let id:Int = Int.random(in: 1..<numberOfMonsters+1)
+        Monster.setImage(UIImage(named: "m\(id)"), for: .normal)
+        health.progress = 1.0
+    }
+    
 
 }
 
