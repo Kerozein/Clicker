eee4d2be2ba0ec4a6fcfa8a34d827ac13421153e
Tom <tomkowalpicchi@gmail.com>
2023-04-17 16:03:41 +0200

    icon + things (strange)

--- Clicker/Clicker/WalkingController.swift
+++ Clicker/Clicker/WalkingController.swift
@@ -12,8 +12,10 @@ import CoreLocation
 class WalkingController: UIViewController, CLLocationManagerDelegate {
     var timer = Timer()
     var isMoving:Bool = false
+    var count = 0
     @IBOutlet weak var moneyLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var footprint: UIImageView!
     let manager = CLLocationManager()
     
     
@@ -23,27 +25,37 @@ class WalkingController: UIViewController, CLLocationManagerDelegate {
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.timerFun), userInfo: nil, repeats: true)
-        // Do any additional setup after loading the view.
+        timerFun()
+        updateMoneyLabel()
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.timerFun), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateMoneyLabel), userInfo: nil, repeats: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
+        // Sert a ne pas utiliser le premier appel du detecteur de vitesse
+        if(count == 0){
+            count = 1
+            return
+        }
+        
         if(location.speed > 2)
         {
+            footprint.image=UIImage(named: "foot_green")
             isMoving=true
-            speedLabel.text = "Vitesse : \(location.speed * 3.6) km/h"
+            speedLabel.text = "Vitesse : \(round(location.speed * 3.6 * 100) / 100.0) km/h"
             Inventory.sharedInstance.money += 5
-            moneyLabel.text = "Argent : \(Inventory.sharedInstance.money)"
         }
     }
     
     @objc func timerFun(){
         if(isMoving == false){
             speedLabel.text = "Vitesse : 0.0 km/h"
-            moneyLabel.text = "Argent : \(Inventory.sharedInstance.money)"
+            footprint.image=UIImage(named: "foot_red")
         }
         else {isMoving = false}
     }
+    
+    @objc func updateMoneyLabel(){moneyLabel.text = "Argent : \(Inventory.sharedInstance.money)$"}
 }
 
---
+++ Clicker/Clicker/Wallet.swift
@@ -0,0 +1,12 @@
+//
+//  Wallet.swift
+//  Clicker
+//
+//  Created by Tom Kowal--Picchi on 17/04/2023.
+//
+
+import UIKit
+
+class Wallet: NSObject {
+
+}
